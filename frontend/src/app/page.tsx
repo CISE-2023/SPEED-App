8f413b02e4bf40fbe60b19ec0e1d6538896b5d95
jWaitoa <jackwaitoa@gmail.com>
2023-10-04 23:00:17 +1300

    style: Styled the homepage and navbar
    Finished the majority of the styling for the homepage but is non-responsive for now.  Changed navbar, and implemented the team's homepage Figma design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,22 @@
+import './styles/globals.scss';
+import styles from './styles/Home.module.css';
+
 export default function Home() {
     return (
         <div>
-        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-        <br />
+            <div className={styles.container}>
+                <h1 className={styles.heading}>SPEED</h1>
+                <h2 className={styles.subheading}>ABOUT</h2>
+                <br />
+                <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod t empor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                aliquip ex ea commodo consequat. Duis aute irure dolor in.
+                </p>
 
-        <h2>[ENSE701 Group 5107]</h2>
+                <button>Search Here</button>
+            </div>
         </div>
     );
 }
