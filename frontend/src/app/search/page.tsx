098e53ddc7a04c305969d6208e6c8d91f991caf0
jWaitoa <jackwaitoa@gmail.com>
2023-10-05 04:29:56 +1300

    style: Finished styling the search page
    Correctly center aligned the two dropdown components and fully styled the search button.

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import style from "../styles/Search.module.css";
+import Button from "../../components/Button";
 
 export default function SearchPage() {
   const [seSelection, setSESelection] = useState("");
@@ -14,52 +15,72 @@ export default function SearchPage() {
         <h1 className="heading">SEARCH</h1>
         <p>Select your software engineering method of interest and claim.</p>
 
-        <form>
-          {/*SE Method dropdown*/}
-          <div className={style.titleContainer}>
-            <h2 className={style.subheading}>SE Method</h2>
-            <h2 className={style.subheading}>Claim</h2>
+        <form className={style.pageContents}>
+          <div className={style.wrapper}>
+            <div className={style.methodWrapper}>
+              <h2 className={style.subheading}>SE Method</h2>
+              {/*SE Method dropdown*/}
+              <div style={{ marginRight: "3vw" }}>
+                <Dropdown id={style.dropdown}>
+                  <Dropdown.Toggle
+                    className="dropdown"
+                    variant="success"
+                    id="dropdown-basic"
+                  >
+                    {seSelection ? seSelection : "Select SE Method"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item
+                      onClick={() => setSESelection("SE Practice 1")}
+                    >
+                      SE Practice 1
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      onClick={() => setSESelection("SE Practice 2")}
+                    >
+                      SE Practice 2
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      onClick={() => setSESelection("SE Practice 3")}
+                    >
+                      SE Practice 3
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </div>
+            <div className={style.spacer} />
+            <div className={style.claimWrapper}>
+              <h2 className={style.subheading} style={{marginLeft: "2.5vw"}}>Claim</h2>
+              <div style={{ marginLeft: "3vw" }}>
+                {/*Claim dropdown*/}
+
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {claimSelection ? claimSelection : "Select Claim"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => setClaimSelection("Claim 1")}>
+                      Claim 1
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => setClaimSelection("Claim 2")}>
+                      Claim 2
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => setClaimSelection("Claim 3")}>
+                      Claim 3
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </div>
           </div>
-          <div className={style.dropdownContainer}>
-            <Dropdown id={style.dropdown}>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                {seSelection ? seSelection : "Select SE Method"}
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setSESelection("SE Practice 1")}>
-                  SE Practice 1
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setSESelection("SE Practice 2")}>
-                  SE Practice 2
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setSESelection("SE Practice 3")}>
-                  SE Practice 3
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            {/*Claim dropdown*/}
-
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                {claimSelection ? claimSelection : "Select Claim"}
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setClaimSelection("Claim 1")}>
-                  Claim 1
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setClaimSelection("Claim 2")}>
-                  Claim 2
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setClaimSelection("Claim 3")}>
-                  Claim 3
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-          <br />
-          <input type="submit" />
+          <input
+            className={style.submit}
+            type="submit"
+            value="Search Article"
+          />
         </form>
       </div>
     </div>
