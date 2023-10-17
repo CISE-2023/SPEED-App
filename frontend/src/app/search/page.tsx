05cdcd7366bb1af0f2814ab3f2f73509d2790c5e
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-10-17 23:55:22 +1300

    feat: on search page the list of dropdowns for the SE Method is now brought in from the database

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,14 +1,62 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import style from "../styles/Search.module.css";
 import "../styles/globals.scss";
+import axios from 'axios';
 
 export default function SearchPage() {
+    // interface for articles
+    interface Article {
+        id: string;
+        title: string;
+        source: string;
+        publication: number;
+        author: string;
+        volume: string;
+        number: number;
+        doi: string;
+        comments: string;
+        summary: string;
+        seMethod: string;
+        claim: string;
+        evidence: string;
+    }
+
+    const [articles, setArticles] = useState<Article[]>([]);
+    const [methods, setMethods] = useState<string[]>([]);
     const [seSelection, setSESelection] = useState("");
     const [claimSelection, setClaimSelection] = useState("");
+
+    // function to get the article data from the backend and filter them based on the search parameters
+    const getArticleData = () => {
+        axios
+            .get('http://localhost:3001/analysis')
+            .then((response) => {
+            const articlesData = response.data;
+      
+            // Extract unique seMethods
+            const uniqueMethods = Array.from(new Set(articlesData.map((article: Article) => article.seMethod)));
+      
+            // Update the methods state
+            setMethods(uniqueMethods as string[]);
+      
+            // Update the articles state with all articles
+            setArticles(articlesData);
+
+            console.log(methods);
+          })
+          .catch((error) => {
+            console.error('Error retrieving a response from the backend server.');
+          });
+      };
+
+      // when page loads, get the article data from the backend and store the unique seMethods in the methods state
+      useEffect(()=>{
+        getArticleData();
+        }, []);
 
     // function to check wether the claim dropdown should be disabled or not
     const check = () => {
@@ -34,8 +82,9 @@ export default function SearchPage() {
 
                 <Dropdown.Menu>
                     <Dropdown.Item onClick={() => {setSESelection(""), setClaimSelection("")}}>All SE Methods</Dropdown.Item>
-                    <Dropdown.Item onClick={() => setSESelection("tdd")}>TDD</Dropdown.Item>
-                    <Dropdown.Item onClick={() => setSESelection("mob programming")}>Mob Programming</Dropdown.Item>
+                    {methods.map((method) => (
+                        <Dropdown.Item key={method} onClick={() => setSESelection(method)}>{method}</Dropdown.Item>
+                    ))}
                 </Dropdown.Menu>
             </Dropdown>  
             <br />
