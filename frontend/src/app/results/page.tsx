2c1e52c3d7555f96a6f5bd1f5e116a7487da3b12
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-10-18 13:47:18 +1300

    feat the search results are displayed in a table.
    You can click buttons above the column to hide or show columns

--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -3,12 +3,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import Image from 'next/image';
 
 const Page = ({ searchParams, }: { searchParams: { seSelection: string;claimSelection: string; }}) => {
 
   // interface for articles
   interface Article {
-    id: string;
+    _id: string;
     title: string;
     source: string;
     publication: number;
@@ -22,8 +24,32 @@ const Page = ({ searchParams, }: { searchParams: { seSelection: string;claimSele
     claim: string;
     evidence: string;
   }
-
   const [articles, setArticles] = useState<Article[]>([]);
+
+  // visibility states for the table columns
+  const [titleVisibility, setTitleVisibility] = useState<boolean>(false);
+  const [authorVisibility, setAuthorVisibility] = useState<boolean>(false);
+  const [publicationVisibility, setPublicationVisibility] = useState<boolean>(false);
+  const [methodVisibility, setMethodVisibility] = useState<boolean>(false);
+  const [claimVisibility, setClaimVisibility] = useState<boolean>(false);
+
+  // functions to toggle the visibility of the table columns
+  const toggleTitlesVisibility = () => () => {
+    setTitleVisibility(!titleVisibility);
+  };
+  const toggleAuthorsVisibility = () => () => {
+    setAuthorVisibility(!authorVisibility);
+  };
+  const togglePublicationsVisibility = () => () => {
+    setPublicationVisibility(!publicationVisibility);
+  };
+  const toggleMethodsVisibility = () => () => {
+    setMethodVisibility(!methodVisibility);
+  };
+  const toggleClaimsVisibility = () => () => {
+    setClaimVisibility(!claimVisibility);
+  };
+
 
   // function to get the article data from the backend and filter them based on the search parameters
   const getArticleData = () => {
@@ -50,16 +76,88 @@ const Page = ({ searchParams, }: { searchParams: { seSelection: string;claimSele
 	}, [])
 
     return (
-      <div>
-      {articles.map((article) => (
-        <div key={article.id} className='text-white'>
-        <h4>Title: {article.title}</h4>
-        <p>Claim: {article.claim}</p>
-        <p>SE Method: {article.seMethod}</p>
-        <p>Publication Year: {article.publication}</p>
-        <p>Author(s): {article.author}</p>
-      </div>
-      ))}
+      <div className="page">
+        <div className="pageContents">
+          <h1 className="heading">Search Results</h1>
+          <p className="text">The following table contains the results of your search.</p>
+
+          {/* table with clickable images to toggle column tables */}
+          <Table>
+            <thead>
+              <tr>
+                <th>
+                  <Image 
+                  src={titleVisibility ? '/mini-arrow-down-svgrepo-com.svg' : '/cross-small-svgrepo-com.svg'}
+                  onClick={toggleTitlesVisibility()} 
+                  alt={''} 
+                  width={20} height={20}
+                  />
+                  Titles
+                </th>
+                <th>
+                  <Image 
+                  src={authorVisibility ? '/mini-arrow-down-svgrepo-com.svg' : '/cross-small-svgrepo-com.svg'}
+                   onClick={toggleAuthorsVisibility()} 
+                   alt={''}
+                   width={20} height={20}
+                   />
+                  Authors
+                </th>
+                <th>
+                  <Image 
+                  src={publicationVisibility ? '/mini-arrow-down-svgrepo-com.svg' : '/cross-small-svgrepo-com.svg'}
+                  onClick={togglePublicationsVisibility()} 
+                  alt={''} 
+                  width={20} height={20}
+                  />
+                  Publication Year
+                </th>
+                <th><Image 
+                src={methodVisibility ? '/mini-arrow-down-svgrepo-com.svg' : '/cross-small-svgrepo-com.svg'}
+                onClick={toggleMethodsVisibility()} 
+                alt={''} 
+                width={20} height={20}
+                />
+                  SE Methods
+                </th>
+                <th>
+                  <Image 
+                  src={claimVisibility ? '/mini-arrow-down-svgrepo-com.svg' : '/cross-small-svgrepo-com.svg'}
+                  onClick={toggleClaimsVisibility()} 
+                  alt={''} 
+                  width={20} 
+                  height={20}/>
+                  Claims
+                </th>
+              </tr>
+            </thead>
+          </Table>
+          <br />
+
+          {/* table with the results */}
+          <Table striped bordered>
+          <thead>
+              <tr>
+                <th hidden={titleVisibility}>Title</th>
+                <th hidden={authorVisibility}>Author(s)</th>
+                <th hidden={publicationVisibility}>Publication Year</th>
+                <th hidden={methodVisibility}>SE Method</th>
+                <th hidden={claimVisibility}>Claim</th>
+              </tr>
+            </thead>
+            <tbody>
+              {articles.map((article) => (
+                <tr key={article._id} className="text">
+                  <td hidden={titleVisibility}>{article.title}</td>
+                  <td hidden={authorVisibility}>{article.author}</td>
+                  <td hidden={publicationVisibility}>{article.publication}</td>
+                  <td hidden={methodVisibility}>{article.seMethod}</td>
+                  <td hidden={claimVisibility}>{article.claim}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
     </div>
   )
 }
